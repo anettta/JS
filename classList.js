a77d7a57f8c3566eb8f04a005bedb6e72db4ed96
Anya <annagapyuk@gmail.com>
2021-05-04 17:52:12 -0400

    classList.js

---
+++ classList.js
@@ -0,0 +1,23 @@
+// className
+// classList
+
+const first = document.getElementById('first');
+const second = document.getElementById('second');
+const third = document.getElementById('third');
+
+// const classValue = first.className; // colors
+
+second.className = 'colors text';
+
+const classValue = third.classList;
+
+third.classList.add('colors', 'text');
+third.classList.remove('text');
+
+let result = third.classList.contains('text');
+
+if (result){
+    console.log('hi there');
+} else {
+      console.log('no class');
+}
