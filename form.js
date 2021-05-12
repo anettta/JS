02c0426112f7dd7f6376c9760a07f187b0040467
Anya <annagapyuk@gmail.com>
2021-05-11 21:21:35 -0400

    added form.js

---
+++ form.js
@@ -0,0 +1,11 @@
+// submit event listener
+
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const password = document.getElementById('password');
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault(); // to access values
+
+});
