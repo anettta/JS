f0c89267014b31a07ea77b61bd2a2c4c2b86f5c5
Anya <annagapyuk@gmail.com>
2021-05-13 13:00:04 -0400

    added variables.js

---
+++ variables.js
@@ -0,0 +1,17 @@
+// variables - most basic building block
+// variables - store, access, modify === value
+// declare, assignment operator, assign value
+
+let name; // undefined
+console.log(name); //nam - error; name - undefined
+
+let lastName = "d";
+console.log(lastItem);
+
+// assign value later, modify existing
+
+let firstName = "Anna";
+let address = "New York";
+let country;
+
+country = "US";
