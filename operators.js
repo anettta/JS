2a104e308d6783a0d1187797edcf93dbe5e8f16c
Anya <annagapyuk@gmail.com>
2021-05-17 06:31:11 -0400

    added comments to operators.js

--- operators.js
+++ operators.js
@@ -1,3 +1,8 @@
+// there are 7 data types in JS: primitive and object
+// primitive: string, number, boolean, symbol, null and undefined
+// object: arrays, functions, objects
+// typeof operator
+
 // Null and Undefined
 // both represent "no value"
 
@@ -9,7 +14,7 @@
 
 // null - "developer sets the value";
 
-let number = 20 + null // 20 + 0;
+let number = 20 + null; // 20 + 0;
 
 // truthy and falsy
 // "", '', ``, 0, -0, NaN, false, null, undefined
@@ -19,15 +24,14 @@ const bool2 = 2 > 1;
 
 const text = ``;
 
-if (text){
-    console.log('the value is truthy');
-
+if (text) {
+  console.log("the value is truthy");
 } else {
-        console.log('the value is falsy');
+  console.log("the value is falsy");
 }
 
 // unary operator - typeof
-let text = 'text';
+let text = "text";
 console.log(typeof text);
 
 // binary operator - assignment
@@ -38,4 +42,3 @@ let num2 = 2 + 6;
 
 const value = 1 < 0;
 value ? console.log("true") : console.log("false");
-
