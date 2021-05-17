905dcb6af755b18c9887d335feea9704919bf447
Anya <annagapyuk@gmail.com>
2021-05-17 15:35:12 -0400

    updated conditional.js

--- conditional.js
+++ conditional.js
@@ -1,2 +1,11 @@
 // Conditional Statements
 // >, <, >=, <=, ==, ===, !=, !==
+
+// equilty
+
+const num1 = 6;
+const num2 = "6";
+
+const value = num1 === num2; // returns false
+
+// === checks for value as well as type
