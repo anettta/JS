fe824553219365b1f55fed1b2cb58959de2e5dc3
Anya <annagapyuk@gmail.com>
2021-05-17 10:02:25 -0400

    added function expression and error function examples to function.js

--- functions.js
+++ functions.js
@@ -1,14 +1,10 @@
-// 1 inch 2.54cm 
+// 1 inch 2.54cm
 
 const wallHeight = 80;
 
 function calculate(value) {
-    return value * 2.54;
+  return value * 2.54;
 }
-
-
-
-
 
 const width = calculate(100);
 const height = calculate(wallHeight);
@@ -16,3 +12,11 @@ const height = calculate(wallHeight);
 const dimensions = [width, height];
 
 console.log(dimensions);
+
+// function expressions
+const add = function (num1, num2) {
+  return num1 + nums2;
+};
+
+// error function
+const multiply = (a, b) => a + b;
