ff105ea7e27c1f601620c52e0594a68e226a2183
Anya <annagapyuk@gmail.com>
2021-05-17 15:46:03 -0400

    updated switch.js

--- switch.js
+++ switch.js
@@ -2,3 +2,16 @@
 // dice values : 1 - 6
 
 const dice = 1;
+
+switch (dice) {
+  case 1:
+    console.log("you got one");
+  case 2:
+    console.log("you got two");
+  case 3:
+    console.log("you got three");
+    break;
+
+  default:
+    break;
+}
