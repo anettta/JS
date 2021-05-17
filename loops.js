ad788ce38eed6128b2c95abb42a5f7295844056a
Anya <annagapyuk@gmail.com>
2021-05-17 15:50:48 -0400

    updated loops.js

--- loops.js
+++ loops.js
@@ -1,27 +1,27 @@
+// loops
+// repeatedly run a block of code while condition is true
+// while loop
+// TURN OFF AUTOSAVE
+
 let amount = 10;
 
-while(amount > 0){
-    console.log('I have  ' + amount + 'dollars and I am going to the mall');
-    amount--;
+while (amount > 0) {
+  console.log("I have  " + amount + "dollars and I am going to the mall");
+  amount--;
 }
-
-
 
 let money = 0;
 
-do{
-    console.log('You have ' + money + 'dollars');
-    money++;
-}
-while(money < 10);
-
-
+do {
+  console.log("You have " + money + "dollars");
+  money++;
+} while (money < 10);
 
 let i;
-for(i = 0; i < 10; i++){
-    console.log('and the number is : ' + i);
+for (i = 0; i < 10; i++) {
+  console.log("and the number is : " + i);
 }
 
-for(let number = 11; number >= 0; number--){
-    console.log('and the number is : ' + number);
+for (let number = 11; number >= 0; number--) {
+  console.log("and the number is : " + number);
 }
