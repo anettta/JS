0d533ccb2ead8368c880dda41f0c759db8f332c0
Anya <annagapyuk@gmail.com>
2021-05-18 14:38:02 -0400

    updated arrayPropertiesAndMathods

--- arrayPropertiesAndMethods.js
+++ arrayPropertiesAndMethods.js
@@ -1,23 +1,21 @@
 // array properties and methods
 
-
 // length
-let names = ['john', 'bobo', 'barry', 'olga', 'ben'];
-console.log(names.length); // 5 
-console.log(names[names.length-1]); // "4" ben. thats why we put -1
+let names = ["anna", "miriam", "barry", "olga", "ben"];
+console.log(names.length); // 5
+console.log(names[names.length - 1]); // "4" ben. thats why we put -1
 
 // concat
-const lastNames = ['pepper', 'onion', 'banana'];
-const allNames = names.concat(lastNames);
+const lastNames = ["smith", "bonjovi", "rytuk"];
+const allNames = names.concat(lastNames); // adds this array to the names array
 console.log(allNames);
 
 // reverse
 
-console.log(allNames.reverse());
-
+console.log(allNames.reverse()); // flips the order
 
 // unshift
-allNames.unshift('susy'); //adds element AT THE BEGINNING of the array
+allNames.unshift("susy"); //adds element AT THE BEGINNING of the array
 
 console.log(allNames);
 
@@ -29,19 +27,17 @@ console.log(allNames);
 
 // push
 
-allNames.push('susy'); //adds an element at the END
+allNames.push("susy"); //adds an element at the END
 console.log(allNames);
 
 // pop
 
-allNames.pop('susy'); // removes an element at the END
+allNames.pop("susy"); // removes an element at the END
 console.log(allNames);
 
 // splice - mutates original array
-const specificNames = allNames.splice(2, 1);  //cut out the specific name(s) from the array
+const specificNames = allNames.splice(2, 1); //cut out the specific name(s) from the array
 console.log(specificNames);
 console.log(allNames);
-
-
-
-
+// splice : starting index and how many items
+// mutates original array [takes it out of the array]
