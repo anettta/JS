282da86f7d3640e6af91c33fd432dd20defd0ed6
Anya <annagapyuk@gmail.com>
2021-05-18 14:18:38 -0400

    updated string_methods.js

--- string_methods.js
+++ string_methods.js
@@ -1,10 +1,20 @@
 // string properties and methods
 // google search :)
 
-let text = "hello there";
+let text = " hello ther";
 
 let result = text.length;
 
+// strings zero based
+console.log(result);
+console.log(text.charAt(text.length - 1));
+console.log(text.indexOf("h")); // case sensitive!
+console.log(text);
+console.log(text.trim()); // get rid of whitespaces at the beginning
+console.log(text.trim().toLowerCase().startsWith("hello"));
+console.log(text.includes(" "));
+console.log(text.slice(2)); // beginning of the string and the rest if second index was not specified
+console.log(text.slice(-2)); // end of the string
 // const person = {
 //   name: "peter",
 //   greeting() {
