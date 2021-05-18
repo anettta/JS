3d9d7a32e48b1ecba45a84e0d3072b78a2f3631d
Anya <annagapyuk@gmail.com>
2021-05-18 17:46:19 -0400

    updated functionEx.js

--- functionsEx.js
+++ functionsEx.js
@@ -1,23 +1,23 @@
 // Functions, return, if, arrays, for loop
 
-const gas = [20,40,100, 30];
+const gas = [20, 40, 100, 30];
 
-const food = [10,40,50];
+const food = [10, 40, 50];
 
-function calculateTotal(arr){
-    let total = 0;
-    for (let i = 0; i < arr.length; i++){
-        total += arr[i];
-    }
-    if(total > 100){
-        console.log(`Whoa! you are spending too much`);
-        return total;
-    }
-    console.log(`you are good`)
-
+function calculateTotal(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // increase by one
+    total += arr[i];
+  }
+  if (total > 100) {
+    console.log(`Whoa! you are spending too much`);
     return total;
-}
+  }
+  console.log(`you are good`);
 
+  return total;
+}
 
 const gasTotal = calculateTotal(gas);
 const foodTotal = calculateTotal(food);
@@ -25,8 +25,8 @@ const foodTotal = calculateTotal(food);
 const randomTotal = calculateTotal([200, 2, 2]);
 
 console.log({
-    gas: gasTotal,
-    food:foodTotal,
-    random:randomTotal
+  // i can pass in an object to the console
+  gas: gasTotal,
+  food: foodTotal,
+  random: randomTotal,
 });
-
