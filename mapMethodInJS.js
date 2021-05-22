a0520b319b26410d6feaa8f86abf9e4c8dc31443
Anya <annagapyuk@gmail.com>
2021-05-21 22:53:07 -0400

    created map.js

---
+++ mapMethodInJS.js
@@ -0,0 +1,14 @@
+// powerful
+// does return a new array
+// does not change size of original array
+// uses values from original array when making new one
+
+const people = [
+  { name: "bob", age: 20, job: "developer" },
+  { name: "peter", age: 25, job: "designer" },
+  { name: "susy", age: 30, job: "manager" },
+];
+
+const ages = people.map(function (person) {
+  // new array since we assigned it to a new variable
+});
