0e1f9029d4c11bfe27e8ab2827bc2df932158a34
Anya <annagapyuk@gmail.com>
2021-05-21 23:07:23 -0400

    updated filter.js

--- filter.js
+++ filter.js
@@ -1,3 +1,14 @@
 // filter does return a new array
 // can manipulate the size of new array
-// returns based on condition
+// because it returns values based on condition
+const people = [
+  { name: "bob", age: 20, job: "developer" },
+  { name: "peter", age: 25, job: "designer" },
+  { name: "susy", age: 30, job: "manager" },
+];
+const youngPeople = people.filter(function (person) {
+  return person.age <= 25; // filter out our array
+});
+const developers = people.filter(function (person) {
+  return person.job === "developer";
+});
