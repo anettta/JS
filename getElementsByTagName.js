73310032bf1f901e74c59b2d2ad0717baa2348fa
Anya <annagapyuk@gmail.com>
2021-05-22 23:04:54 -0400

    updated getElementsByTagName.js

--- getElementsByTagName.js
+++ getElementsByTagName.js
@@ -1,2 +1,10 @@
 // getElementsByTagName('tagname');
 const headings = document.getElementsByTagName("h2");
+headings[0].style.color = "red"; // first index
+
+const items = document.getElementsByTagName("li"); // NODE LIST
+items[2].style.color = "orange";
+const betterItems = [...items]; // ARRAY
+betterItems.forEach(function (item) {
+  console.log(item);
+});
