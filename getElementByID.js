0f53cdc4a6c80aafa3ca3c6216dfba18338d34ae
Anya <annagapyuk@gmail.com>
2021-05-22 22:56:20 -0400

    updated getElementByID.js

---
+++ getElementByID.js
@@ -0,0 +1,8 @@
+// select the element or group of elements
+// decide what changes will be applied
+// getElementById('element');
+
+const h1 = document.getElementById("title"); // have access to a node object
+h1.style.color = "red";
+
+document.getElementById("btn").style.backgroundColor = "blue";
