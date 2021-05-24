e01464380a78332727224de9df67a910d8499075
Anya <annagapyuk@gmail.com>
2021-05-24 14:34:50 -0400

    updated prevNextElemSibling.js

--- prevNextElemSibling.js
+++ prevNextElemSibling.js
@@ -1,3 +1,10 @@
 // previousElementSibling
 // nextElementSibling
 // you skip the whitespace using above
+
+// nodeValue
+const item = document.getElementById("special");
+const value = item.firstChild.nodeValue;
+
+// textContent
+const easyValue = item.textContent;
