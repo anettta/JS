bced128013d3af675f508d52532d873569742a23
Anya <annagapyuk@gmail.com>
2021-05-24 14:45:46 -0400

    updated get_set_attributes.js

--- get_set_attributes.js
+++ get_set_attributes.js
@@ -1,17 +1,18 @@
 // getAttribute(), setAttribute()
 
-const first = document.querySelector('.value');
-const classValue = first.getAttribute('class');
+const first = document.querySelector(".value");
+const classValue = first.getAttribute("class");
 // console.log(classValue);
 
-const link = document.getElementById('special');
-const showLink = link.getAttribute('id');
+const link = document.getElementById("special");
+const showLink = link.getAttribute("id");
 console.log(showLink);
 
 const last = link.nextElementSibling;
-last.setAttribute('class', 'first');
-last.textContent = 'i also have a class of first';
+// dynamically added class of first and text content below
+last.setAttribute("class", "first");
+last.textContent = "i also have a class of first";
 console.log(last);
 
-const links = document.querySelectorAll('.first');
+const links = document.querySelectorAll(".first");
 console.log(links);
