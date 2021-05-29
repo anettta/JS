e98513071f1acc09925ae7e61df985a25168d779
Anya <annagapyuk@gmail.com>
2021-05-29 11:52:55 -0400

    added comment

--- classList.js
+++ classList.js
@@ -12,7 +12,7 @@ second.className = "colors text";
 
 const classValue = third.classList;
 
-third.classList.add("colors", "text"); // add comma
+third.classList.add("colors", "text"); // add comma to use multiple classes
 third.classList.remove("text");
 
 let result = third.classList.contains("text");
